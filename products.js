9ffe33fd515f3c20894239352214447f88658b42
behzad <90694618+behzadiv@users.noreply.github.com>
2023-07-20 09:30:37 +0330

    add productData as static and local data

---
+++ products.js
@@ -0,0 +1,50 @@
+export const productsData = [
+  {
+    id: 1,
+    title: "پک پذیرایی ترحیم 1",
+    imageUrl: "./images/behpax-funeral-pack1.jpg",
+    price: "35000",
+  },
+  {
+    id: 2,
+    title: "پک پذیرایی ترحیم 2",
+    imageUrl: "./images/behpax-funeral-pack2.jpg",
+    price: "32000",
+  },
+  {
+    id: 3,
+    title: "پک پذیرایی ترحیم 3",
+    imageUrl: "./images/behpax-funeral-pack3.jpg",
+    price: "32000",
+  },
+  {
+    id: 4,
+    title: "پک پذیرایی ترحیم 4",
+    imageUrl: "./images/behpax-funeral-pack4.jpg",
+    price: "38000",
+  },
+  {
+    id: 5,
+    title: "پک پذیرایی همایش 1",
+    imageUrl: "./images/behpax-meeting-pack1.jpg",
+    price: "32000",
+  },
+  {
+    id: 6,
+    title: "پک پذیرایی همایش 2",
+    imageUrl: "./images/behpax-meeting-pack2.jpg",
+    price: "34000",
+  },
+  {
+    id: 7,
+    title: "پک پذیرایی همایش 3",
+    imageUrl: "./images/behpax-meeting-pack3.jpg",
+    price: "37000",
+  },
+  {
+    id: 8,
+    title: "پک پذیرایی همایش 4",
+    imageUrl: "./images/behpax-meeting-pack4.jpg",
+    price: "30000",
+  },
+];
