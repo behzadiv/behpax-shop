4880ae91cc62d2595ecc73c18f1ff1db722837cf
behzad <90694618+behzadiv@users.noreply.github.com>
2023-07-20 10:02:04 +0330

    execute class programming - get & show products

---
+++ app.js
@@ -0,0 +1,43 @@
+import { productsData } from "./products.js";
+
+// 1.get products
+
+class Products {
+  getProducts() {
+    return productsData;
+  }
+}
+
+// 2.display products
+
+class Ui {
+  displayProducts(productsData) {
+    let result = "";
+    productsData.forEach((product) => {
+      result += `
+        <div class="product-container" id="productId">
+          <img class="product-img" src="${product.imageUrl}" alt="" />
+          <div class="product-description">
+            <h5 class="product-title">${product.title}</h5>
+            <span class="product-price">${product.price} تومان </span>
+          </div>
+            <button class="productAdd-btn" id=${product.id}>
+            <i class="fa fa-cart-plus fa-lg"></i>
+             افزودن به سبد خرید
+             </button>
+        </div>`;
+    });
+    document.querySelector(".products-list").innerHTML = result;
+  }
+}
+
+// 3.storage
+
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const products = new Products();
+  const productsData = products.getProducts();
+  const ui = new Ui();
+  ui.displayProducts(productsData);
+});
